7cd37ec3205c78b54f0c60a279904e16986f3890
Surender Reddy Adavalli <vikas.adavelli@gmail.com>
2024-11-22 14:01:33 +0530

    Initial commit for maven project

---
+++ src/main/java/in/kmit/sample2/App.java
@@ -0,0 +1,13 @@
+package in.kmit.sample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
